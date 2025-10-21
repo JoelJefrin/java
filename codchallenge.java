0d103e3347591c2107d92c74f74391d90a9bd50f
Joel Jefrin <joeljefrin0@gmail.com>
2025-10-21 15:26:18 +0530

    THird Commit

---
+++ codchallenge.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class codchallenge {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            int mark = scan.nextInt();
+            if (mark>=35){
+                System.out.println("You Passed");
+
+            }
+            else{
+                System.out.println("You failed");
+            }
+
+    }
+    
+}
--- practice.java
+++ practice.java
@@ -1,21 +1,31 @@
 
 import java.util.Scanner;
-
 public class practice {
     public static void main(String[] args) {
-        try( Scanner scan = new Scanner(System.in)){
-            String name = scan.nextLine();
-            int age = scan.nextInt();
-            scan.nextLine();
-            String address = scan.nextLine();
-            System.out.println("My Name is "+ name);
-            System.out.println("My Age is "+ age);
-            System.out.println("My address is "+ address);
+        // try( Scanner scan = new Scanner(System.in)){
+        //     String name = scan.nextLine();
+        //     int age = scan.nextInt();
+        //     scan.nextLine();
+        //     String address = scan.nextLine();
+        //     System.out.println("My Name is "+ name);
+        //     System.out.println("My Age is "+ age);
+        //     System.out.println("My address is "+ address);
 
 
-        }
-        String joel = "name";
-        System.out.println(joel.charAt(2));
+        // }
+        // String joel = "name";
+        // System.out.println(joel.charAt(2));
+        
+        Scanner scan = new Scanner(System.in);
+            String RCB = scan.nextLine();
+            if (RCB.equals("win")){
+                System.out.println("ee sala cup namde");
+
+            }
+            else{
+                System.out.println("we will see nxt year");
+            }
+
         
     }
     
