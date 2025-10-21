f4092514e1e6eeef1215e45e0371e5122554372e
Joel Jefrin <joeljefrin0@gmail.com>
2025-10-21 14:23:07 +0530

    Second Commit

---
+++ strcomp.java
@@ -0,0 +1,13 @@
+public class strcomp {
+    public static void main(String[] args) {
+        String f1 = new String("apple");
+        String f2 = new String("apple");
+
+        String a1 = "orange";
+        String a2 = "orange";
+
+        System.out.println(f1==f2);
+        System.out.println(f1.equals(f2));  //use always this .equals()
+        System.out.println(a1==a2);
+    }
+}
