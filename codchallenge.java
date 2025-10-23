c1655d6e7dc4383ff0eafca3e97cd6d94b3654f7
Joel Jefrin <joeljefrin0@gmail.com>
2025-10-23 22:39:45 +0530

    .

--- codchallenge.java
+++ codchallenge.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
 
 public class codchallenge {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        // Scanner scan = new Scanner(System.in);
             // int income = scan.nextInt();
             // if (income>=7000){
             //     System.out.println("Scholership Available");
@@ -11,15 +10,26 @@ public class codchallenge {
             // else{
             //     System.out.println("Scholership not available");
             // }
-            int num = scan.nextInt();
-            if(num%3==0 && num % 5==0){
-                System.out.println("The Number is Divisible by 3 and 5");
+            // int num = scan.nextInt();
+            // if(num%3==0 && num % 5==0){
+            //     System.out.println("The Number is Divisible by 3 and 5");
+
+            // }
+            // else {
+            //     System.out.println("The Number is Not Divisible by 3 and 5");
+            // }
+            int oddcount= 0;
+            for (int i=1;i<=10;i=i+1){
+                if(i%2==1){
+                    
+                    oddcount =oddcount+1;
+                    System.out.println("Odd Number : "+i);
+                }
 
             }
-            else {
-                System.out.println("The Number is Not Divisible by 3 and 5");
-            }
-
+            System.out.println("The Number of Odd numbers present Between 1 to 10 is: "+oddcount);
+           
+           
     }
-    
+     
 }
