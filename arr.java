2a765efb09f4b0b1e993fc587ebca00d2333fed3
Joel Jefrin <joeljefrin0@gmail.com>
2025-10-24 16:08:00 +0530

    .

---
+++ arr.java
@@ -0,0 +1,10 @@
+public class arr {
+    public static void main(String[] args) {
+        String[] playlist = new String[3];
+
+        playlist[1]= "song 1";
+      
+        System.out.println(playlist[1]);
+    }
+    
+}
